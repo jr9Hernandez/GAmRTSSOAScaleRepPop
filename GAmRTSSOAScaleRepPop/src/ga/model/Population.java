d54c0913fbe84581454e5f39a17dc10ffb291048
Julian Mariño <richardes71@gmail.com>
2018-08-17 15:11:02 -0300

    more changes

--- GAmRTSSOAScaleRepPop/src/ga/model/Population.java
+++ GAmRTSSOAScaleRepPop/src/ga/model/Population.java
@@ -45,10 +45,10 @@ public class Population {
 	}
 	
 	public void addChromosome(Chromosome chromosome){
-		int id = rand.nextInt(5000);
+		int id = rand.nextInt(100000);
 		//check if the id exist or not
 		while(idExist(id)){
-			id = rand.nextInt(5000);
+			id = rand.nextInt(100000);
 		}
 		
 		ChromosomeValue tChrom = new ChromosomeValue(id, chromosome, BigDecimal.ZERO);
